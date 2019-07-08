dffb73b4cb54278333a5fb38ef6f844d4a5203f3
DESMOND OO JYAN ANN <TP041318@mail.apu.edu.my>
2019-07-08 14:02:02 +0800

    Add project files.

---
+++ LETSGOGIT/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace LETSGOGIT.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
